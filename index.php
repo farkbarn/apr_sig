4ea23eca59e830955e283d41293fc73101fd8990
Frank Barrera | @farkbarn <frankb2320@gmail.com>
2015-07-12 17:32:54 -0430

    agregando comentarios para red social facebook

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * The main template file.
- *
- * @package eryn
  */
 get_header(); ?>
 
@@ -14,7 +12,18 @@ get_header(); ?>
                 get_template_part('content');
             endif;
         endwhile; 
-    ?>     
+    ?> 
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-width="100%" data-numposts="10"></div>
+
     <?php eryn_paging_nav(); ?>
 
 <?php get_footer(); ?>
