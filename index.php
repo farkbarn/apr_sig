3d007d508776e6ba9b25617b76c335193063b0e2
Frank Barrera | @farkbarn <frankb2320@gmail.com>
2015-07-12 18:22:34 -0430

    modificando comentarios en fb

--- index.php
+++ index.php
@@ -13,17 +13,6 @@ get_header(); ?>
             endif;
         endwhile; 
     ?> 
-    <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-width="100%" data-numposts="10"></div>
-
     <?php eryn_paging_nav(); ?>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -7,7 +7,6 @@
 
 get_header(); ?>
 
-
     <?php while ( have_posts() ) : the_post(); ?>
         <?php get_template_part( 'content' ); ?>	
         <?php
@@ -17,7 +16,17 @@ get_header(); ?>
             endif;
         ?>
     <?php endwhile; // end of the loop. ?>
+<div id="fb-root"></div>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
+		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-num-posts="10" data-width="100%"></div>
 
 	<?php 
 		if(get_theme_mod('eryn_global_sidebar_posts')):
