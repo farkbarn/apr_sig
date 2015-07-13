a4fab493253724c0b0da18e5e3f477c9b53ef998
Frank Barrera | farkbarn <frankb2320@gmail.com>
2015-07-13 15:12:22 -0430

    actualización

--- comments.php
+++ comments.php
@@ -13,9 +13,18 @@
  * return early without loading the comments.
  */
 if (true) {
-	echo "esto es una prueba";
-} else {
 
+<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
+		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-num-posts="10" data-width="465"></div>
+
+} else {
 
 if ( post_password_required() ) {
 	return;
